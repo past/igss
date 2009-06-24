1d6f7660ae5781bff0d71c039b2607e0c3be0142
Panagiotis Astithas <pastith@gmail.com>
2009-06-25 00:26:58 +0300

    Move the GSS API stuff into a separate file, gss.js. This should eventually turn into a full GSS API library.

---
+++ iGSS.dcproj/project/gss.js
@@ -0,0 +1,260 @@
+/* -*- mode: JavaScript; c-basic-offset: 4; tab-width: 4; indent-tabs-mode: nil -*- */
+/* ex: set tabstop=4 expandtab: */
+/*
+ * Copyright (c) 2008 Panagiotis Astithas, Christos V. Stathis
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+var gss = {
+    // The current user's username.
+    username: '',
+    // The current user's authentication token.
+    token: '',
+    // The root URL of the REST API.
+    GSS_URL: 'http://pithos.grnet.gr/pithos/rest',
+    // The user root namespace.
+    root: {},
+    // The files and folders in the current directory.
+    items: [],
+
+    // Creates a HMAC-SHA1 signature of method+time+resource, using the authentication token.
+    sign: function (method, time, resource, auth_token) {
+        var q = resource.indexOf('?');
+        var res = q == -1? resource: resource.substring(0, q);
+        var data = method + time + res;
+        // Use strict RFC compliance
+        b64pad = "=";
+        return b64_hmac_sha1(atob(auth_token), data);
+    },
+
+    // A helper function for making API requests.
+    sendRequest: function (handler, method, resource, modified, file, form, update) {
+        var loading = document.getElementById('activityIndicator').object;
+        loading.startAnimation();
+        // If the resource is an absolute URI, remove the GSS_URL.
+        if (resource.indexOf(gss.GSS_URL) == 0)
+            resource = resource.slice(gss.GSS_URL.length, resource.length);
+        var now = (new Date()).toUTCString();
+        var sig = gss.sign(method, now, resource, gss.token);
+        var params = null;
+        if (form)
+            params = form;
+        else if (update)
+            params = update;
+
+        var req = new XMLHttpRequest();
+        req.open(method, gss.GSS_URL + resource, true);
+        req.onreadystatechange = function (event) {
+            if (req.readyState == 4) {
+                loading.stopAnimation();
+                if(req.status == 200) {
+                    handler(req);
+                } else {
+                    alert("Error fetching data: HTTP status " + req.status+" ("+req.statusText+")");
+                }
+            }
+        };
+        req.setRequestHeader("Authorization", gss.username + " " + sig);
+        req.setRequestHeader("X-GSS-Date", now);
+        if (modified)
+            req.setRequestHeader("If-Modified-Since", modified);
+
+        if (file) {
+            req.setRequestHeader("Content-Type", "text/plain");
+            req.setRequestHeader("Content-Length", file.length);
+        } else if (form) {
+            req.setRequestHeader("Content-Length", params.length);
+            req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;");
+        } else if (update) {
+            req.setRequestHeader("Content-Length", params.length);
+            req.setRequestHeader("Content-Type", "application/json;");
+        }
+
+        if (!file)
+            req.send(params);
+        else
+            req.send(file);
+    },
+
+    // Fetches the 'user' namespace.
+    fetchUser: function () {
+        gss.sendRequest(gss.parseUser, 'GET', '/' + gss.username + '/');
+    },
+
+    // Parses the 'user' namespace response.
+    parseUser: function (req) {
+        gss.root = JSON.parse(req.responseText);
+        gss.items = [];
+        gss.items.push({name: 'Files', location: gss.root.fileroot});
+        gss.items.push({name: 'Trash', location: gss.root.trash});
+        gss.items.push({name: 'Shared', location: gss.root.shared});
+        gss.items.push({name: 'Others', location: gss.root.others});
+        gss.items.push({name: 'Groups', location: gss.root.groups});
+        var list = document.getElementById('list').object;
+        list.reloadData();
+        var name = document.getElementById('name');
+        name.innerHTML = gss.root.name;
+    },
+
+    // Fetches the 'files' namespace.
+    fetchFiles: function (event) {
+        gss.sendRequest(gss.parseFiles, 'GET', gss.root.fileroot);
+    },
+
+    // Parses the 'files' namespace response.
+    parseFiles: function (req) {
+        var filesobj = JSON.parse(req.responseText);
+        gss.items = [];
+        var folders = filesobj.folders;
+        while (folders.length > 0) {
+            var folder = folders.pop();
+            gss.items.push({name: folder.name+'/', location: folder.uri});
+        }
+        var files = filesobj.files;
+        while (files.length > 0) {
+            var file = files.pop();
+            gss.items.push({name: file.name, location: file.uri, owner: file.owner, data: file});
+        }
+        var list = document.getElementById('list').object;
+        list.reloadData();
+        var browser = document.getElementById('browser').object;
+        browser.goForward(document.getElementById('listLevel'), 'Files');
+    },
+
+    // Fetches the 'trash' namespace.
+    fetchTrash: function (event) {
+        gss.sendRequest(gss.parseTrash, 'GET', gss.root.trash);
+    },
+
+    // Parses the 'trash' namespace response.
+    parseTrash: function (req) {
+        var filesobj = JSON.parse(req.responseText);
+        gss.items = [];
+        var folders = filesobj.folders;
+        while (folders.length > 0) {
+            var folder = folders.pop();
+            gss.items.push({name: folder.name, location: folder.uri});
+        }
+        var files = filesobj.files;
+        while (files.length > 0) {
+            var file = files.pop();
+            gss.items.push({name: file.name, location: file.uri, data: file});
+        }
+        var list = document.getElementById('list').object;
+        list.reloadData();
+        var browser = document.getElementById('browser').object;
+        browser.goForward(document.getElementById('listLevel'), 'Trash');
+    },
+
+    // Fetches the 'others' namespace.
+    fetchOthers: function (event) {
+        gss.sendRequest(gss.parseOthers, 'GET', gss.root.others);
+    },
+
+    // Parses the 'others' namespace response.
+    parseOthers: function (req) {
+        var users = JSON.parse(req.responseText);
+        gss.items = [];
+        while (users.length > 0) {
+            var user = users.pop();
+            gss.items.push({name: user.username, location: user.uri, owner: user.username});
+        }
+        var list = document.getElementById('list').object;
+        list.reloadData();
+        var browser = document.getElementById('browser').object;
+        browser.goForward(document.getElementById('listLevel'), 'Others');
+    },
+
+    // Parses the 'groups' namespace response.
+    fetchGroups: function (event) {
+        gss.sendRequest(gss.parseGroups, 'GET', gss.root.groups);
+    },
+
+    // Parses the 'groups' namespace response.
+    parseGroups: function (req) {
+        var groups = JSON.parse(req.responseText);
+        gss.items = [];
+        while (groups.length > 0) {
+            var group = groups.pop();
+            gss.items.push({name: group.name, location: group.uri, owner: username});
+        }
+        var list = document.getElementById('list').object;
+        list.reloadData();
+        var browser = document.getElementById('browser').object;
+        browser.goForward(document.getElementById('listLevel'), 'Groups');
+    },
+
+    // Fetches the specified file.
+    fetchFile: function (file) {
+        gss.sendRequest(gss.parseFile, 'HEAD', file);
+    },
+
+    // Parses the response for a file request.
+    parseFile: function (req) {
+        var headers = gss.parseHeaders(req);
+        var file = JSON.parse(headers['X-Gss-Metadata']);
+        detailController.populate(file);
+        var browser = document.getElementById('browser').object;
+        browser.goForward(document.getElementById('detailLevel'), file.name);
+    },
+
+    // A helper function that parses the HTTP headers from the specified XHR and returns them in a map.
+    parseHeaders: function (req) {
+        var allHeaders = req.getAllResponseHeaders();
+        var headers = {};
+        var ls = /^\s*/;
+        var ts = /\s*$/;
+        alert('Headers: '+allHeaders);
+        var lines = allHeaders.split("\n");
+        for (var i = 0; i < lines.length; i++) {
+            var line = lines[i];
+            if (line.length == 0) continue;
+            var pos = line.indexOf(':');
+            var name = line.substring(0, pos).replace(ls, "").replace(ts, "");
+            var value = line.substring(pos + 1).replace(ls, "").replace(ts, "");
+            headers[name] = value;
+        }
+        return headers;
+    },
+
+    // Fetches the specified folder.
+    fetchFolder: function (folder) {
+        gss.sendRequest(gss.parseFolder, 'GET', folder);
+    },
+
+    // Parses the response for a folder request.
+    parseFolder: function (req) {
+        var folder = JSON.parse(req.responseText);
+        gss.items = [];
+        var folders = folder.folders;
+        while (folders.length > 0) {
+            var f = folders.pop();
+            gss.items.push({name: f.name+'/', location: f.uri});
+        }
+        var files = folder.files;
+        while (files.length > 0) {
+            var file = files.pop();
+            gss.items.push({name: file.name, location: file.uri, owner: file.owner, data: file});
+        }
+        var list = document.getElementById('list').object;
+        list.reloadData();
+        var backHandler = function() {
+            gss.fetchFolder(folder.parent);
+        };
+        var browser = document.getElementById('browser').object;
+        browser.goForward(document.getElementById('listLevel'), folder.name, backHandler);
+    }
+    
+};
+
--- iGSS.dcproj/project/main.js
+++ iGSS.dcproj/project/main.js
@@ -8,12 +8,6 @@
 var username;
 // The current user's authentication token.
 var token;
-// The root URL of the REST API.
-var GSS_URL = 'http://pithos.grnet.gr/pithos/rest';
-// The user root namespace.
-var root;
-// The container for the list items.
-var items = [];
 
 var listController = {
     // This object acts as a controller for the list UI.
@@ -21,37 +15,37 @@ var listController = {
     
     numberOfRows: function() {
         // The List calls this dataSource method to find out how many rows should be in the list.
-        return items.length;
+        return gss.items.length;
     },
     
     prepareRow: function(rowElement, rowIndex, templateElements) {
         // The List calls this dataSource method for every row.  templateElements contains references to all elements inside the template that have an id. We use it to fill in the text of the rowTitle element.
         if (templateElements.rowTitle) {
-            templateElements.rowTitle.innerText = items[rowIndex].name;
+            templateElements.rowTitle.innerText = gss.items[rowIndex].name;
         }
 
         // We also assign an onclick handler that will cause the browser to go to the next page.
         var self = this;
         var handler = function() {
-            var item = items[rowIndex];
-            detailController.populate(item);
+            var item = gss.items[rowIndex];
             var browser = document.getElementById('browser').object;
             if (item.name == 'Files')
-                fetchFiles();
+                gss.fetchFiles();
             else if (item.name == 'Trash')
-                fetchTrash();
+                gss.fetchTrash();
             else if (item.name == 'Shared')
-                fetchShared();
+                gss.fetchShared();
             else if (item.name == 'Others')
-                fetchOthers();
+                gss.fetchOthers();
             else if (item.name == 'Groups')
-                fetchGroups();
-            else if (item.data)
+                gss.fetchGroups();
+            else if (item.data) {
                 // File
-                fetchFile(item.location);
-            else
+                detailController.populate(item.data);
+                gss.fetchFile(item.location);
+            } else
                 // Folder
-                fetchFolder(item.location);
+                gss.fetchFolder(item.location);
         };
         rowElement.onclick = handler;
     }
@@ -69,18 +63,18 @@ var detailController = {
         var detailOwner = document.getElementById('detailOwner');
         detailOwner.innerHTML = this._item.owner;
         var detailDescription = document.getElementById('detailDescription');
-        
-        var resource = this._item.folder + this._item.name;
+        alert(JSON.stringify(this._item));
+        var resource = this._item.folder.uri + this._item.name;
         // If the resource is an absolute URI, remove the GSS_URL.
-        if (resource.indexOf(GSS_URL) == 0)
-            resource = resource.slice(GSS_URL.length, resource.length);
+        if (resource.indexOf(gss.GSS_URL) == 0)
+            resource = resource.slice(gss.GSS_URL.length, resource.length);
         resource = decodeURI(resource);
         var now = (new Date()).toUTCString();
-        var sig = sign('GET', now, resource, token);
+        var sig = gss.sign('GET', now, resource, gss.token);
         var authparam = encodeURI("Authorization=" + this._item.owner + " " + sig);
         var dateparam = encodeURI("Date=" + now);
         var param = "?" + authparam + "&" + dateparam;
-        detailDescription.innerHTML = "<a href='" + this._item.folder + this._item.name + param + "'>Download</a>";
+        detailDescription.innerHTML = "<a href='" + this._item.folder.uri + this._item.name + param + "'>Download</a>";
     }
     
 };
@@ -107,216 +101,11 @@ function load() {
         }
     }
     dashcode.setupParts();
-    fetchUser();
+    gss.username = username;
+    gss.token = token;
+    gss.fetchUser();
     var loading = document.getElementById('activityIndicator').object;
     loading.stopAnimation();
-}
-
-// Creates a HMAC-SHA1 signature of method+time+resource, using the token.
-function sign(method, time, resource, token) {
-	var q = resource.indexOf('?');
-	var res = q == -1? resource: resource.substring(0, q);
-	var data = method + time + res;
-	// Use strict RFC compliance
-	b64pad = "=";
-	return b64_hmac_sha1(atob(token), data);
-}
-
-// A helper function for making API requests.
-function sendRequest(handler, method, resource, modified, file, form, update) {
-    var loading = document.getElementById('activityIndicator').object;
-    loading.startAnimation();
-    // If the resource is an absolute URI, remove the GSS_URL.
-    if (resource.indexOf(GSS_URL) == 0)
-        resource = resource.slice(GSS_URL.length, resource.length);
-    resource = resource;
-	var now = (new Date()).toUTCString();
-    var sig = sign(method, now, resource, token);
-	var params = null;
-	if (form)
-		params = form;
-	else if (update)
-		params = update;
-
-	var req = new XMLHttpRequest();
-	req.open(method, GSS_URL + resource, true);
-	req.onreadystatechange = function (event) {
-		if (req.readyState == 4) {
-            loading.stopAnimation();
-            if(req.status == 200) {
-                handler(req);
-		    } else {
-		    	alert("Error fetching data: HTTP status " + req.status+" ("+req.statusText+")");
-		    }
-		}
-	}
-	req.setRequestHeader("Authorization", username + " " + sig);
-	req.setRequestHeader("X-GSS-Date", now);
-	if (modified)
-		req.setRequestHeader("If-Modified-Since", modified);
-
-	if (file) {
-		req.setRequestHeader("Content-Type", "text/plain");
-		req.setRequestHeader("Content-Length", file.length);
-	} else if (form) {
-		req.setRequestHeader("Content-Length", params.length);
-	    req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;");
-	} else if (update) {
-		req.setRequestHeader("Content-Length", params.length);
-	    req.setRequestHeader("Content-Type", "application/json;");
-	}
-
-	if (!file)
-		req.send(params);
-	else
-		req.send(file);
-}
-
-// Fetches the 'user' namespace.
-function fetchUser()
-{
-    sendRequest(parseUser, 'GET', '/'+username+'/');
-}
-
-// Parses the 'user' namespace response.
-function parseUser(req) {
-    root = JSON.parse(req.responseText);
-    items = [];
-    items.push({name: 'Files', location: root['fileroot']});
-    items.push({name: 'Trash', location: root['trash']});
-    items.push({name: 'Shared', location: root['shared']});
-    items.push({name: 'Others', location: root['others']});
-    items.push({name: 'Groups', location: root['groups']});
-    var list = document.getElementById('list').object;
-    list.reloadData();
-    var name = document.getElementById('name');
-    name.innerHTML = root['name'];
-}
-
-// Fetches the 'files' namespace.
-function fetchFiles(event)
-{
-    sendRequest(parseFiles, 'GET', root['fileroot']);
-}
-
-// Parses the 'files' namespace response.
-function parseFiles(req) {
-    var filesobj = JSON.parse(req.responseText);
-    items = [];
-    var folders = filesobj['folders'];
-    while (folders.length > 0) {
-        var folder = folders.pop();
-        items.push({name: folder['name']+'/', location: folder['uri']});
-    }
-    var files = filesobj['files'];
-    while (files.length > 0) {
-        var file = files.pop();
-        items.push({name: file['name'], location: file['uri'], owner: file['owner'], data: file});
-    }
-    var list = document.getElementById('list').object;
-    list.reloadData();
-    var browser = document.getElementById('browser').object;
-    browser.goForward(document.getElementById('listLevel'), 'Files');
-}
-
-// Fetches the 'trash' namespace.
-function fetchTrash(event)
-{
-    sendRequest(parseTrash, 'GET', root['trash']);
-}
-
-// Parses the 'trash' namespace response.
-function parseTrash(req) {
-    var filesobj = JSON.parse(req.responseText);
-    items = [];
-    var folders = filesobj['folders'];
-    while (folders.length > 0) {
-        var folder = folders.pop();
-        items.push({name: folder['name'], location: folder['uri']});
-    }
-    var files = filesobj['files'];
-    while (files.length > 0) {
-        var file = files.pop();
-        items.push({name: file['name'], location: file['uri'], data: file});
-    }
-    var list = document.getElementById('list').object;
-    list.reloadData();
-    var browser = document.getElementById('browser').object;
-    browser.goForward(document.getElementById('listLevel'), 'Trash');
-}
-
-// Fetches the 'others' namespace.
-function fetchOthers(event)
-{
-    sendRequest(parseOthers, 'GET', root['others']);
-}
-
-// Parses the 'others' namespace response.
-function parseOthers(req) {
-    var users = JSON.parse(req.responseText);
-    items = [];
-    while (users.length > 0) {
-        var user = users.pop();
-        items.push({name: user['username'], location: user['uri'], owner: user['username']});
-    }
-    var list = document.getElementById('list').object;
-    list.reloadData();
-    var browser = document.getElementById('browser').object;
-    browser.goForward(document.getElementById('listLevel'), 'Others');
-}
-
-// Parses the 'groups' namespace response.
-function fetchGroups(event)
-{
-    sendRequest(parseGroups, 'GET', root['groups']);
-}
-
-// Parses the 'groups' namespace response.
-function parseGroups(req) {
-    var groups = JSON.parse(req.responseText);
-    items = [];
-    while (groups.length > 0) {
-        var group = groups.pop();
-        items.push({name: group['name'], location: group['uri'], owner: username});
-    }
-    var list = document.getElementById('list').object;
-    list.reloadData();
-    var browser = document.getElementById('browser').object;
-    browser.goForward(document.getElementById('listLevel'), 'Groups');
-}
-
-// Fetches the specified file.
-function fetchFile(file)
-{
-    sendRequest(parseFile, 'HEAD', file);
-}
-
-// Parses the response for a file request.
-function parseFile(req) {
-    var headers = parseHeaders(req);
-    var file = JSON.parse(headers['X-Gss-Metadata']);
-    detailController.populate(file);
-    var browser = document.getElementById('browser').object;
-    browser.goForward(document.getElementById('detailLevel'), file.name);
-}
-
-// A helper function that parses the HTTP headers from the specified XHR and returns them in a map.
-function parseHeaders(req) {
-    var allHeaders = req.getAllResponseHeaders();
-    var headers = {};
-    var ls = /^\s*/;
-    var ts = /\s*$/;
-    
-    var lines = allHeaders.split("\n");
-    for (var i = 0; i < lines.length; i++) {
-        var line = lines[i];
-        if (line.length == 0) continue;
-        var pos = line.indexOf(':');
-        var name = line.substring(0, pos).replace(ls, "").replace(ts, "");
-        var value = line.substring(pos + 1).replace(ls, "").replace(ts, "");
-        headers[name] = value;
-    }
-    return headers;
 }
 
 // A function that moves the browser to a new list view.
@@ -327,31 +116,3 @@ function parseHeaders(req) {
     browser.goForward(div, name, function() {setTimeout(function() {stack.removeView(div);}, 750);});
 }*/
 
-// Fetches the specified folder.
-function fetchFolder(folder)
-{
-    sendRequest(parseFolder, 'GET', folder);
-}
-
-// Parses the response for a folder request.
-function parseFolder(req) {
-    var folder = JSON.parse(req.responseText);
-    items = [];
-    var folders = folder['folders'];
-    while (folders.length > 0) {
-        var f = folders.pop();
-        items.push({name: f['name']+'/', location: f['uri']});
-    }
-    var files = folder['files'];
-    while (files.length > 0) {
-        var file = files.pop();
-        items.push({name: file['name'], location: file['uri'], owner: file['owner'], data: file});
-    }
-    var list = document.getElementById('list').object;
-    list.reloadData();
-    var backHandler = function() {
-        fetchFolder(folder['parent']);
-    };
-    var browser = document.getElementById('browser').object;
-    browser.goForward(document.getElementById('listLevel'), folder.name, backHandler);
-}
